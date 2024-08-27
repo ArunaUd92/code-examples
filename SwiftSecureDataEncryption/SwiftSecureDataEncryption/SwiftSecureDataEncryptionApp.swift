e15287ee70f5327fbd2241d9d5475be241e03b64
Aruna Udayanga <arunaudayanga@Arunas-MacBook-Pro.local>
2024-08-27 05:56:57 +0100

    Add code sample for SwiftSecureDataEncryption

---
+++ SwiftSecureDataEncryption/SwiftSecureDataEncryption/SwiftSecureDataEncryptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSecureDataEncryptionApp.swift
+//  SwiftSecureDataEncryption
+//
+//  Created by Aruna Udayanga on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSecureDataEncryptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
